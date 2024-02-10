5b1be4468dce4fd2adf421edec15dedaca0d5255
Nidou0 <nidallehocine@gmail.com>
2024-02-10 12:25:36 +0800

    updates requested

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "success@manju.com";
+$php_main_email = "success@manjuappathurai.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
